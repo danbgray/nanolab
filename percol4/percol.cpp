56f53afbc89f506e0dd4a70491505d01453645e5
Dmitry Baksheev <bd@mail.ru>
2017-03-26 10:13:11 +0700

    Added write_voltages

--- percol4/percol.cpp
+++ percol4/percol.cpp
@@ -293,6 +293,17 @@ class Grid {
             }
         }
     }
+    void write_voltages(const std::string& filename) {
+        std::ofstream out(filename);
+        for (Int c = 0; c < N - 1; ++c) {
+            for (Int r = 0; r < M; ++r) {
+                if (nodes[at(r, c)]) {
+                    REAL v = c == 0 ? VLEFT : c == N -1 ? VRIGHT : vx.at({r, c});
+                    out << c << " " << M - 1 - r << " " << v << std::endl;
+                }
+            }
+        }
+    }
     REAL left_current() {
         REAL res = 0;
         for (Int r = 0; r < M; ++r) {
@@ -428,6 +439,7 @@ void dwim1(Int M, Int N, Int seed, REAL frac1) {
             lc, rc, (lc - rc) / std::max(std::abs(lc), std::abs(rc)),
             human_readable_time(seconds(t1 - t0)).c_str());
         if (!failed) grid.write_currents("cur.dat");
+        if (!failed) grid.write_voltages("volt.dat");
     }
 }
 
